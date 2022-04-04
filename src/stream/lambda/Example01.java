b11b2308347ce640bfcfd640554c5430b055b3c7
tmdgusya <tmdgusya@naver.com>
2022-04-04 12:08:22 +0900

    Lambda 식 Example01 추가

--- src/stream/lambda/Example01.java
+++ src/stream/lambda/Example01.java
@@ -7,9 +7,9 @@ public class Example01 {
 
     public static void main(String[] args) {
         IntStream intStream = IntStream.of(1,3,4,2,1,6,7);
-
+        IntStream intStream2 = IntStream.of(1,3,4,2,1,6,7);
         intStream.filter(ele -> ele != 1).forEach(System.out::println);
-        intStream.filter(Example01::predicate).forEach(System.out::println);
+        intStream2.filter(Example01::predicate).forEach(System.out::println);
     }
 
     public static boolean predicate(int ele) {
